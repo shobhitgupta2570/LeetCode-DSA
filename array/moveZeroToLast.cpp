2f32cc08307c1a7c063127638e0a83e241701411
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2024-12-05 23:57:51 +0530

    move zero to last

---
+++ array/moveZeroToLast.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int lastNonZeroFoundAt = 0; // Pointer for the last non-zero element found
+
+//         // Move all non-zero elements to the front
+//         for (int current = 0; current < nums.size(); ++current) {
+//             if (nums[current] != 0) {
+//                 nums[lastNonZeroFoundAt] = nums[current];
+//                 lastNonZeroFoundAt++;
+//             }
+//         }
+
+//         // Fill the remaining positions with zeros
+//         for (int i = lastNonZeroFoundAt; i < nums.size(); ++i) {
+//             nums[i] = 0;
+//         }
+//     }
+};
