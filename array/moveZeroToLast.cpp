ddae12ff28acbbd40dc716954a72b2a5bb1de0c0
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-01-15 22:29:37 +0530

    move zeroes to last

--- array/moveZeroToLast.cpp
+++ array/moveZeroToLast.cpp
@@ -17,3 +17,23 @@
 //         }
 //     }
 // };
+
+// class Solution {
+// public:
+//     void moveZeroes(vector<int>& nums) {
+//         int fZero = -1 ;
+//         for(int i=0; i<nums.size(); i++){
+//             if(nums[i]==0){
+//                  fZero = i ;
+//                  break;
+//             }
+//         }
+//         if(fZero != -1){
+//         for(int i=fZero+1; i<nums.size(); i++){
+//             if(nums[i]!=0){
+//                 swap(nums[i],nums[fZero]);
+//                 fZero++;
+//             }
+//         }}
+//     }
+// };
