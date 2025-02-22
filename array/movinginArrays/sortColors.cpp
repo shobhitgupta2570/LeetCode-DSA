9f7b9112fe95ff7f4719a18465a970aa91939b5b
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-02-22 22:57:59 +0530

    sort Colors

--- array/movinginArrays/sortColors.cpp
+++ array/movinginArrays/sortColors.cpp
@@ -1,27 +1,21 @@
 // class Solution {
 //     public:
 //         void sortColors(vector<int>& nums) {
-//             int cnt0=0;
-//             int cnt1=0;
-//             int cnt2=0;
-//             for(int i=0; i<nums.size(); i++){
-//                 if(nums[i]==0) cnt0++;
-//                 else if(nums[i]==1) cnt1++;
-//                 else cnt2++;
-//             }
-//             for(int i=0; i<nums.size(); i++){
-//                 if(cnt0){
-//                     nums[i] = 0;
-//                     cnt0--;
+//             int low=0, mid=0, high=nums.size()-1;
+//             while(mid<=high){
+//                 if(nums[mid]==0){
+//                     swap(nums[low],nums[mid]);
+//                     low++;
+//                     mid++;
 //                 }
-//                 else if(cnt1){
-//                     nums[i] = 1;
-//                     cnt1--;
+//                 else if(nums[mid]==1){
+//                     mid++;
 //                 }
 //                 else{
-//                     nums[i] = 2;
-//                     cnt2--;
+//                     swap(nums[high],nums[mid]);
+//                     high--;
 //                 }
 //             }
+            
 //         }
 //     };
---
+++ recursion/combinationSum/combination.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+//     private:
+//         void findCombo(int ind, int n, int k, vector<int>& ds, vector<vector<int>>& ans){
+//             if(k==0){
+//                 ans.push_back(ds);
+//                 return;
+//             }
+//             if(ind>n) return;
+//             for(int i = ind; i<=n; i++){
+//                 ds.push_back(i);
+//                 findCombo(i+1,n,k-1,ds,ans);
+//                 ds.pop_back();
+//             }
+//         }
+    
+//     public:
+//         vector<vector<int>> combine(int n, int k) {
+//             vector<vector<int>> ans;
+//             vector<int> ds;
+//             findCombo(1,n,k,ds,ans);
+//             return ans;
+//         }
+//     };
