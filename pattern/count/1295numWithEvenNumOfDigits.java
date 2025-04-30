c6a7f28a94fab7cee48be45b7d8c0a5de255ecea
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-04-30 21:01:46 +0530

    1295 number with even number of digits

---
+++ pattern/count/1295numWithEvenNumOfDigits.java
@@ -0,0 +1,12 @@
+// class Solution {
+//     public int findNumbers(int[] nums) {
+//         int evenDigits = 0;
+
+//         for(int num : nums){
+//             int length = String.valueOf(num).length();
+//             if(length%2 == 0) evenDigits++;
+//         }
+
+//         return evenDigits;
+//     }
+// }
