d758292ea124ebf60e369bcf3824a45efc7dc530
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-08-15 11:25:13 +0530

    342 Power Of 2

---
+++ miscellaneous/power/342Powrof2.cpp
@@ -0,0 +1,12 @@
+// class Solution {
+// public:
+//     bool isPowerOfFour(int n) {
+//     //     int rem = 2147483644
+//     //    return 2147483644%n==0 ? true : false;
+//     if(n==1) return true;
+//     while(n%4==0 && n>4){
+//         n = n/4;
+//     }
+//     return n == 4;
+//     }
+// };
