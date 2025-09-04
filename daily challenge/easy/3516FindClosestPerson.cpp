903e899897645638b2665b8c621f3da2a4c65c83
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-09-04 07:17:27 +0530

    3516 Find Closest Person

---
+++ daily challenge/easy/3516FindClosestPerson.cpp
@@ -0,0 +1,8 @@
+// class Solution {
+// public:
+//     int findClosest(int x, int y, int z) {
+//         if(abs(y-z) > abs(z-x)) return 1;
+//         else if(abs(y-z) == abs(z-x)) return 0;
+//         else return 2;
+//     }
+// };
