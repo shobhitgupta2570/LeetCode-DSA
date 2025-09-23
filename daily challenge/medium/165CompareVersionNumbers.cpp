3704b45129c7010f99d1167e5c6609e26fbc1118
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-09-23 08:00:47 +0530

    165 Compare Version Numbers

---
+++ daily challenge/medium/165CompareVersionNumbers.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int compareVersion(string version1, string version2) {
+        int n = version1.size(), m = version2.size();
+        int i = 0, j = 0;
+
+        while (i < n || j < m) {
+            long num1 = 0, num2 = 0;
+
+            // extract next number from version1
+            while (i < n && version1[i] != '.') {
+                num1 = num1 * 10 + (version1[i] - '0');
+                i++;
+            }
+
+            // extract next number from version2
+            while (j < m && version2[j] != '.') {
+                num2 = num2 * 10 + (version2[j] - '0');
+                j++;
+            }
+
+            if (num1 > num2) return 1;
+            if (num1 < num2) return -1;
+
+            // skip the dot
+            i++;
+            j++;
+        }
+
+        return 0;
+    }
+};
