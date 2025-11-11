67d8cade80aebb6bf5a4d05db3909bc4be936119
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2025-11-11 07:48:34 +0530

    Leetcode 474 Ones And Zeroes

---
+++ daily challenge/medium/dp/leetcode474OnesAndZeroes.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        
+        for (string& s : strs) {
+            int zeros = count(s.begin(), s.end(), '0');
+            int ones = s.size() - zeros;
+
+            // Traverse backwards to avoid recomputing using same item
+            for (int i = m; i >= zeros; --i) {
+                for (int j = n; j >= ones; --j) {
+                    dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
