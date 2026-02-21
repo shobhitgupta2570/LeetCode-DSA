bbca65e4a0a8f42c7f132a4d3b3a4c5cd1d76507
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2026-02-21 21:26:54 +0530

    762 Prime Number Of Set Bits In Binary Representation

---
+++ bits/easy/762PrimeNumberOfSetBitsInBinaryRepresentation.cpp
@@ -0,0 +1,8 @@
+int countPrimeSetBits(int L, int R) {
+        int cnt = 0, hash[20] = {0, 0, 1, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1};
+        for (int i = L; i <= R; i++) {
+            bitset<20> b(i);
+            if(hash[b.count()]) cnt++;
+        }
+        return cnt;
+    }
