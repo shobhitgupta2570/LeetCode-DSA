0daf399eb90b6a0287b974e083de8509f325ef80
SHOBHIT GUPTA <shobhitgupta2570@gmail.com>
2026-07-15 08:36:15 +0530

    3658 GCD Of Odd And Even Numbers

---
+++ math/easy/3658GCDOfOddAndEvenNumbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    int gcd(int a, int b){
+        while(b > 0){
+        int temp = b;
+        b = a%b;
+        a = temp;
+        }
+        return a;
+    }
+public:
+    int gcdOfOddEvenSums(int n) {
+        int odd = n*n;
+        int even = n*(n+1);
+        return gcd(odd,even);
+    }
+};
